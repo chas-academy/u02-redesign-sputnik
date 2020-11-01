e469787166a6d91194e4b8b205bee5ce866e7b80
Fijo-Lurk <filip.johansson@chasacademy.se>
2020-11-01 21:14:20 +0100

    added my own js script for keypress for hamburgermenu and added onkeypress function in html

--- js/hamburgermenu.js
+++ js/hamburgermenu.js
@@ -1,7 +1,13 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
+
+
+function enterKeyFunc() {
+  navbarLinks.classList.toggle('active')
+}
